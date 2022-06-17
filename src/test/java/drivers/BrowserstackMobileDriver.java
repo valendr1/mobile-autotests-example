e6b5d27ee6f7cb459361a5cc278a5b90cf5b2ce5
Valery <andrvaler95@gmail.com>
2022-06-18 01:12:48 +0300

    added the mobile autotest

---
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -0,0 +1,50 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserstackMobileDriver implements WebDriverProvider {
+
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+    static String userName = config.user();
+    static String password = config.key();
+    static String appBrowserstack = config.app();
+
+    @Override
+    public WebDriver createDriver(Capabilities capabilities) {
+        MutableCapabilities mutableCapabilities = new MutableCapabilities();
+        mutableCapabilities.merge(capabilities);
+        // Set your access credentials
+        mutableCapabilities.setCapability("browserstack.user", userName);
+        mutableCapabilities.setCapability("browserstack.key", password);
+
+        // Set URL of the application under test
+        mutableCapabilities.setCapability("app", appBrowserstack);
+
+        // Specify device and os_version for testing
+        mutableCapabilities.setCapability("device", "Google Pixel 3");
+        mutableCapabilities.setCapability("os_version", "9.0");
+
+        // Set other BrowserStack capabilities
+        mutableCapabilities.setCapability("project", "Mobile Autotest Example");
+        mutableCapabilities.setCapability("build", "browserstack-build-1");
+        mutableCapabilities.setCapability("name", "selenide android test");
+        return new RemoteWebDriver(getBrowserstackUrl(), mutableCapabilities);
+    }
+
+    public static URL getBrowserstackUrl() {
+        try {
+            return new URL("http://hub.browserstack.com/wd/hub");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
